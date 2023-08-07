a890a3763038efc93561d606bc9306534ca96b03
Cristian Narvaezn <cdnarvaez92@misena.edu.co>
2023-08-08 01:32:11 +0200

    actualizacion cadena de conexioin2

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://perreke3010:<perreke3010>@cluster0.shcqvr3.mongodb.net/Alumnos', {
+mongoose.connect('mongodb+srv://perreke3010:perreke3010@cluster0.shcqvr3.mongodb.net/Alumnos?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
